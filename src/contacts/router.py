55d00938b7336f26ade0370384a9c5406d15ad44
Nik Kor <3357381Nk@gmail.com>
2024-12-03 14:51:33 +0000

    Add endpoints to get all contacts

--- src/contacts/router.py
+++ src/contacts/router.py
@@ -4,11 +4,22 @@ from src.contacts.schemas import Contact, ContactResponse
 
 router = APIRouter()
 
+
+# Add new contact
 @router.post("/contact")
 async def create_contact(contact: Contact) -> ContactResponse:
     return ContactResponse(first_name=contact.name, last_name=contact.surname)
 
 
+# Get all contacts
+@router.get("/contact")
+async def get_contacts(
+    limit: int = Query(10, description="The number of contacts you want to retrieve")
+):
+    return {"limit": limit}
+
+
+# Get a specific contact(by ID)
 @router.get("/contact/{contact_id}")
 async def get_contact(
     contact_id: int = Path(

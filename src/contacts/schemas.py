760940488eb8d204a918872d199a332469c4abb8
Nik Kor <3357381Nk@gmail.com>
2024-12-04 00:03:01 +0000

    Minor bug with schemas fixed

--- src/contacts/schemas.py
+++ src/contacts/schemas.py
@@ -1,22 +1,26 @@
 from pydantic import BaseModel, EmailStr
 from datetime import date
 
+
 class Contact(BaseModel):
     name: str
     surname: str
     email: EmailStr
     phone: int
     date_of_birth: date
-    additional_info: str|None = None
-    
+    additional_info: str | None = None
+
+
 class ContactResponse(Contact):
     id: int
-    
+
     class Config:
         from_atributes = True
-        
-class ContactCreate(BaseModel):
+
+
+class ContactCreate(Contact):
     pass
 
-class ContactUpdate(BaseModel):
+
+class ContactUpdate(Contact):
     pass
